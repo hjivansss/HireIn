e44056a7e0dc65b4af2da7320fbbef11ae271ed3
H Jivan Singha <hjivansingha123@gmail.com>
2026-07-17 14:16:57 +0530

    added a sidebar

---
+++ src/pages/Recruiter_Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from '../components/sidebar'
+const Recruiter = () => {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default Recruiter
